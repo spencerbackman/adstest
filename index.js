ad0ce1b7558ec30481283e19b02bfd4cedca20d7
Spencer <backmanspencer99@gmail.com>
2019-05-02 13:31:08 -0500

    added deploy stuff for heroku

--- index.js
+++ index.js
@@ -11,51 +11,9 @@ app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-const bizSdk = require('facebook-nodejs-business-sdk');
-const Ad = bizSdk.Ad;
-const AdAccount = bizSdk.AdAccount;
-const Business = bizSdk.Business;
-const Campaign = bizSdk.Campaign;
-const accountId = 'act_2430591390316845';
-const accessToken = 'EAAIO2Ea8s5kBACqs1U1ndkthckNQ76JNnKksvS5fZBzHjKxigaCcAflnHD4xsGd1vpH2D5TY8fZAZANpIphXsDfmLnZAGbg8EE5hQ2vZCZAULU6rVTag3fJvMJbLWZAOl66B5fuQPoOdHZAdYDZCocCAOhfvzJCOnhW7Ahi0nm3Ki5QZDZD';
-const bussinessId = '686769051709194'; //ADD business ID here.
-const campaignId = '23843230381800145'; //ADD Campaign ID Here
-const api = bizSdk.FacebookAdsApi.init(accessToken);
-const account = new AdAccount(accountId);
-
-const insightsFields = ['impressions', 'frequency', 'clicks', 'ctr'];
-
-const req = account.read([AdAccount.Fields.name])
-.then((account) => {
-    return account.getCampaigns([Campaign.Fields.name], {limit: 10});
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 })
-.then((result) => {
-    const campaignIds = result.map((campaign) => {
-        return campaign.id;
-    });
-    const params = Object.assign({
-        level: 'ad',
-        breakdowns: ['dma']
-    }, {});
-    const campaignsAdsInsightsFields = insightsFields.concat('campaign_id');
-    return account.getInsights(campaignsAdsInsightsFields, params);
-})
-.then((insights) => {
-    return insights
-}).catch(err => {
-    console.log(err);
-})
-
-
-app.use('/fbApi', (req, res) => {
-    console.log(req.body)
-    console.log(res.body)
-}
-);
-
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// })
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
